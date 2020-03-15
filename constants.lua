72a6f413752047fb794e987304851ba6b2120680
Sharlikran <danielhmpdx16@gmail.com>
2020-03-14 20:27:57 -0700

    He forgot to change the version

--- constants.lua
+++ constants.lua
@@ -4,7 +4,7 @@ local AF = AdvancedFilters
 --Addon base variables
 AF.name = "AdvancedFilters"
 AF.author = "ingeniousclown, Randactyl, Baertram (current)"
-AF.version = "1.5.3.4"
+AF.version = "1.5.3.6"
 AF.savedVarsVersion = 1.511
 AF.website = "http://www.esoui.com/downloads/info245-AdvancedFilters.html"
 AF.feedback = "https://www.esoui.com/portal.php?id=136&a=faq"
